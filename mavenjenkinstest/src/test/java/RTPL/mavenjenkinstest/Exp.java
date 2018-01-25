0074c51a38a66bb156a8a87581e82cd972f6d87a
Ashish <aashishvyass@gmail.com>
2018-01-26 03:34:02 +0530

    first commit

---
+++ mavenjenkinstest/src/test/java/RTPL/mavenjenkinstest/Exp.java
@@ -0,0 +1,41 @@
+package RTPL.mavenjenkinstest;
+
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+ public class Exp
+ {
+//Testfdfdf 
+@Test
+public void testgooglrsearch(){
+	//System.setProperty("webdriver.gecko.driver", "F://geckodriver-v0.13.0-win64//geckodriver.exe");
+
+	System.setProperty("webdriver.chrome.driver", "F://MY DOWNLOADS//chromedriver_win32//chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+
+	//WebDriver driver = new FirefoxDriver();
+//it will open the goggle page
+driver.get("http://google.in");
+
+//we expect the title �Google � should be present
+String Expectedtitle = "Google";
+
+//it will fetch the actual title
+String Actualtitle = driver.getTitle();
+
+System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+
+//it will compare actual title and expected title
+
+Assert.assertEquals(Actualtitle, Expectedtitle);
+//print out the result
+
+System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+
+ }
+
+}
+
